ba344f3d3961eba2096378236b34a7ce9bea0fff
mohit-kumar-jalan <mohit@toothsi.in>
2023-05-09 01:52:50 +0530

    loader commit

--- src/containers/ProductListing.tsx
+++ src/containers/ProductListing.tsx
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { setProducts } from "../redux/actions/productsActions";
 import ProductComponent from "./ProductComponent";
+import { ClipLoader } from "react-spinners";
 
 const ProductPage = () => {
+  const [loading, setLoading] = useState(true);
   const products = useSelector((state: any) => state.allProducts.products);
   const dispatch = useDispatch();
   const fetchProducts = async () => {
@@ -13,6 +15,9 @@ const ProductPage = () => {
       .catch((err) => {
         console.log("Err: ", err);
       });
+    if (response?.data) {
+      setLoading(false);
+    }
     dispatch(setProducts(response?.data));
   };
 
@@ -23,9 +28,19 @@ const ProductPage = () => {
   console.log("Products :", products);
   return (
     <>
-      <div className="ui grid container">
-        <ProductComponent />
-      </div>
+      {loading ? (
+        <div
+          className="ui container"
+          style={{ marginTop: 100, textAlign: "center" }}
+        >
+          <ClipLoader color="#36d7b7" />
+          <div>please wait while dummy api is loading the data.</div>
+        </div>
+      ) : (
+        <div className="ui grid container">
+          <ProductComponent />
+        </div>
+      )}
     </>
   );
 };
